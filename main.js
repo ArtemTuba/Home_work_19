c8019b162578add83cfea0cac8106b9a6d805408
ArtemTuba <tuba9521@gmail.com>
2023-01-10 21:29:56 +0200

    Fix home work

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function randomImg(){
-    return Math.floor(Math.random() * 8);   
+    console.log(Math.floor(Math.random() * 8 + 1))
+    return Math.floor(Math.random() * 8 + 1);   
 }
 
 document.getElementById("images").innerHTML = '<img src="img/'+randomImg()+'.jpg"/>';
+
+
+
+
